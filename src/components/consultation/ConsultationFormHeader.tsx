eefb93a22707a0fb7f8500dc96a84371a43d6968
Arthu133 <ahndias@sga.pucminas.br>
2025-05-26 20:24:57 -0300

    att

--- src/components/consultation/ConsultationFormHeader.tsx
+++ src/components/consultation/ConsultationFormHeader.tsx
@@ -6,7 +6,7 @@ const ConsultationFormHeader: React.FC = () => {
     <>
       <h2 className="text-2xl font-bold mb-6 text-center">Consultoria Personalizada em Criptomoedas</h2>
       <p className="text-gray-600 mb-6 text-center">
-        Preencha o formulário abaixo e receba orientações personalizadas sobre criptomoedas diretamente no Telegram!
+        Preencha o formulário abaixo e receba orientações personalizadas sobre criptomoedas diretamente no Whatsapp!
       </p>
       <div className="bg-blue-50 border-l-4 border-blue-500 p-4 mb-6">
         <div className="flex">
